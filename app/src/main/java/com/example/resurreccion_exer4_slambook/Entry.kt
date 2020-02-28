dedf3be9e8925037d6ef00d777d6a8f7d4633953
fcresurreccion <fcresurreccion@up.edu.ph>
2020-02-28 18:54:39 +0800

    docs: add docs to code

--- app/src/main/java/com/example/resurreccion_exer4_slambook/Entry.kt
+++ app/src/main/java/com/example/resurreccion_exer4_slambook/Entry.kt
@@ -1,3 +1,6 @@
 package com.example.resurreccion_exer4_slambook
 
+/*
+ * create data class Entry with parameters name, nickname and age
+ */
 data class Entry(var name: String = "", var nickname: String = "", var age: String = "")
--- app/src/main/java/com/example/resurreccion_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/resurreccion_exer4_slambook/MainActivity.kt
@@ -10,39 +10,76 @@ import com.example.resurreccion_exer4_slambook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    /*
+     * declare binding variable
+     */
     private lateinit var binding: ActivityMainBinding
 
+    /*
+     * create a variable for the instance
+     * of the data class Entry
+     */
     private val entry: Entry = Entry()
 
+    /*
+     * override onCreate function
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        /*
+         * create binding object
+         */
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        /*
+         * set the value of the entry variable
+         * created in the layout file
+         */
         binding.entry = entry
 
+        /*
+         * set onlick listener of done button
+         */
         binding.doneButton.setOnClickListener {
             addEntry(it)
         }
     }
 
+    /*
+     * create onclick handler for done button
+     * which displays the name, nickname and age
+     * entered by the user
+     */
     private fun addEntry(view : View){
         binding.apply{
+            /*
+             * update visibility of ui components
+             */
             scrollView.visibility = View.GONE
-
             displayNameTextView.visibility = View.VISIBLE
             displayNicknameTextView.visibility = View.VISIBLE
             displayAgeTextView.visibility = View.VISIBLE
             thanksTextView.visibility = View.VISIBLE
 
+            /*
+             * update the name, nickname and age text views
+             * with the details entered by the user
+             */
             entry?.name = nameEditText.text.toString()
             entry?.nickname = nicknameEditText.text.toString()
             entry?.age = ageEditText.text.toString()
 
+            /*
+             * add invalidateAll() so that the UI is refreshed
+             * with the value in the updated binding object
+             */
             invalidateAll()
         }
 
-        // Hide keyboard
+        /*
+         * hide the keyboard
+         */
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
