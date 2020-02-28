668180b9323ca8e5a1dae35d11831f81d63f1b79
fcresurreccion <fcresurreccion@up.edu.ph>
2020-02-28 12:12:10 +0800

    feat: add onclick handler for done button and text views for display

--- app/src/main/java/com/example/resurreccion_exer4_slambook/Entry.kt
+++ app/src/main/java/com/example/resurreccion_exer4_slambook/Entry.kt
@@ -1,16 +1,3 @@
 package com.example.resurreccion_exer4_slambook
 
-import android.provider.ContactsContract
 
-data class Entry(
-    var name: String="",
-    var nickname: String="",
-    var age: Int,
-    var birthday: String="",
-    var course: String="",
-    var phone: ContactsContract.CommonDataKinds.Phone,
-    var color: String="",
-    var dream: String="",
-    var crush: String="",
-    var message: String=""
-)
--- app/src/main/java/com/example/resurreccion_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/resurreccion_exer4_slambook/MainActivity.kt
@@ -1,39 +1,59 @@
 package com.example.resurreccion_exer4_slambook
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.databinding.DataBindingUtil
-import com.example.resurreccion_exer4_slambook.databinding.ActivityMainBinding
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-//    private val entry: Entry = Entry("","",0,"","",0,"","","","")
+    private lateinit var doneButton: Button
+    private lateinit var scrollView: View
+    private lateinit var displayNameView: TextView
+    private lateinit var displayNicknameView: TextView
+    private lateinit var displayAgeView: TextView
+    private lateinit var editNameView: EditText
+    private lateinit var editNicknameView: EditText
+    private lateinit var editAgeView: EditText
+    private lateinit var displayThanksView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-//        binding.entry = entry
-//
-//        binding.doneButton.setOnClickListener {
-//            addEntry(it)
-//        }
+        doneButton = findViewById(R.id.done_button)
+        scrollView = findViewById(R.id.scrollView)
+        displayNameView = findViewById(R.id.displayName_textView)
+        displayNicknameView = findViewById(R.id.displayNickname_textView)
+        displayAgeView = findViewById(R.id.displayAge_textView)
+        displayThanksView = findViewById(R.id.thanks_textView)
+        editNameView = findViewById(R.id.name_editText)
+        editNicknameView = findViewById(R.id.nickname_editText)
+        editAgeView = findViewById(R.id.age_editText)
+
+        doneButton.setOnClickListener {
+            addEntry(it)
+        }
     }
 
     private fun addEntry(view : View){
-//        binding.apply {
-//            entry?.name = nameEditText.text.toString()
-//            entry?.nickname = nicknameEditText.text.toString()
-//            entry?.age = ageEditText.text
-//            entry?.birthday = birthdayEditText.text.toString()
-//            entry?.name = nameEditText.text.toString()
-//            entry?.name = nameEditText.text.toString()
-//            entry?.name = nameEditText.text.toString()
-//            entry?.name = nameEditText.text.toString()
-//            entry?.name = nameEditText.text.toString()
-//            entry?.name = nameEditText.text.toString()
-//        }
+        scrollView.visibility = View.GONE
+
+        displayNameView.text = editNameView.text
+        displayNicknameView.text = editNicknameView.text
+        displayAgeView.text = editAgeView.text
+
+        displayNameView.visibility = View.VISIBLE
+        displayNicknameView.visibility = View.VISIBLE
+        displayAgeView.visibility = View.VISIBLE
+        displayThanksView.visibility = View.VISIBLE
+
+        // Hide keyboard
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
